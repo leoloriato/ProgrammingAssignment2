c53b2260bab25f0a1f52eb886f4cbf4c07c169e1
Leonardo Amato Loriato <amatol@tcu.gov.br>
2019-05-20 11:11:57 -0300

    Assignment 2 completed!
    Implemented makeCacheMatrix and cacheSolve functions acording to task instructions.
    They cache an inverse matrix computation, retrieving this value in order
    to save CPU computation.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,42 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Creates an object to cache an inverse matrix, returning a list of functions
+## to manipulate this cache
 
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function() m
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
+## Computes the inverse matrix, returning the cached inversed matrix, if it 
+## has already been calculated, or executing this mathematical operation
+## (and storing this operation in the cache)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached inverse matrix")
+        return(inv)
+    }
+    data <- x$get()
+    
+    # Creating an indetity matrix
+    n <- nrow(data)
+    matrix.indentity <- matrix(0, n, n)
+    diag(matrix.indentity) <- 1
+    
+    # Computing the inverse matrix
+    inv <- solve(data, matrix.indentity)
+    inv
 }
